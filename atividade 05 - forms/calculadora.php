ec132c7b39d5b5c9b72061817d26c9b658a011ad
Vitor Felde <feldevitor@gmail.com>
2026-08-20 22:07:19 -0300

    Update calculadora.php

--- atividade 05 - forms/calculadora.php
+++ atividade 05 - forms/calculadora.php
@@ -1,3 +1,44 @@
+<?php
+
+$erro = "";
+
+if(isset($_POST["num1"])) { //verificando se entrou algum valor no num1 com o isset
+//fazemos isso para que o erro nao apareça assim que a pagina carregar ele so aparecera se nao houver nada inserido nas operações 
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["operacao"];
+
+    if ($num1 == "" || $num2 == "") {
+        $erro = "Preencha os dois campos para poder realizar a operação!";
+    }
+
+    else {
+
+    if ($op == 'soma'){
+        $som = $num1 + $num2;
+        echo "Resultado da Operação: " . $som;
+        }
+
+    else if ($op == 'sub'){
+        $sub = $num1 - $num2;
+        echo "Resultado da Operação: " .$sub;
+        }
+
+    else if ($op == 'div'){
+        $div = $num1 / $num2;
+        echo "Resultado da Operação: " .$div;
+        }
+
+    else if ($op == 'mul'){
+        $mul = $num1 * $num2;
+        echo "Resultado da Operação: " .$mul;
+        }
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -25,40 +66,11 @@
         <br><br>
     </form>
 
-<?php
+    <div>
+        <?php echo $erro; ?>
+    </div>
 
-$num1 = $_POST["num1"];
-$num2 = $_POST["num2"];
-$op = $_POST["operacao"];
 
-if ($num1 == null || $num2 == null) {
-    echo "Preencha os dois campos para poder realizar a operação!";
-}
-
-else {
-
-if ($op == 'soma'){
-    $som = $num1 + $num2;
-    echo "Resultado da Operação: " . $som;
-    }
-
-else if ($op == 'sub'){
-    $sub = $num1 - $num2;
-    echo "Resultado da Operação: " .$sub;
-    }
-
-else if ($op == 'div'){
-    $div = $num1 / $num2;
-    echo "Resultado da Operação: " .$div;
-    }
-
-else if ($op == 'mul'){
-    $mul = $num1 * $num2;
-    echo "Resultado da Operação: " .$mul;
-    }
-}
-
-?>
 
 </body>
 </html>
