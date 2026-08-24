1340f8ba0c85ddbbde434428abb6eb2aae1a6864
Vitor Felde <feldevitor@gmail.com>
2026-08-24 13:44:48 -0300

    Update atividadeava.php

--- atividade 04 - clientserver/atividadeava.php
+++ atividade 04 - clientserver/atividadeava.php
@@ -9,15 +9,24 @@ Caso os 3 parâmetros tenham sido enviados, exiba a progressão aritmética requ
 contrário, exiba um mensagem informando qual ou quais parâmetros não foram informados.
 Dica: o próximo termo da progressão aritmética é calculado pela soma do termo anterior e da razão*/
 
-$ini = $_GET["inicio"];
-$raz = $_GET["razao"];
-$qtd = $_GET["quantidade"];
-$som = 0;
-
-if (isset($_GET["inicio"]) && ($_GET["razao"]) && ($_GET["quantidade"])) {
+if (isset($_GET["inicio"]) && isset($_GET["razao"]) && isset($_GET["quantidade"])) {
+	$ini = $_GET["inicio"];
+	$raz = $_GET["razao"];
+	$qtd = $_GET["quantidade"];
 	for ($i = 0; $i < $qtd; $i++) {
-		$som = $ini + $som + $raz; 
+		echo $ini . " ";
+		$ini = $ini + $raz; 
 	}
 }
 
-echo $som;
+	if (!isset ($_GET["inicio"])) { //se for diferente do que o isset quer (receber algum valor), passara a mensagem mto top
+	echo "Você esqueceu de passar o inicio" . "<br>";
+}
+
+	if (!isset ($_GET["razao"])) {
+	echo "Você esqueceu de passar a razão" . "<br>";
+}
+
+	if (!isset ($_GET["quantidade"])) {
+	echo "Você esqueceu de passar a quantidade" . "<br>";
+}
