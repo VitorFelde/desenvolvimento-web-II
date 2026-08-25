56ecb0afb3932644e4d978b06fde1aaf77c589ab
Vitor Felde <feldevitor@gmail.com>
2026-08-25 13:02:50 -0300

    Update atividadeava.php

--- atividade 04 - clientserver/atividadeava.php
+++ atividade 04 - clientserver/atividadeava.php
@@ -9,7 +9,9 @@ Caso os 3 parâmetros tenham sido enviados, exiba a progressão aritmética requ
 contrário, exiba um mensagem informando qual ou quais parâmetros não foram informados.
 Dica: o próximo termo da progressão aritmética é calculado pela soma do termo anterior e da razão*/
 
-if (isset($_GET["inicio"]) && isset($_GET["razao"]) && isset($_GET["quantidade"])) {
+
+//aqui fazemos dessa forma que vai verificar se existe ou se for diferente de vazio podera entrar no calculo, se nao entra nos ifs la embaixo
+if (isset($_GET["inicio"]) && $_GET["inicio"] != "" && isset($_GET["razao"]) && $_GET["razao"] != "" && isset($_GET["quantidade"]) && ($_GET["quantidade"] != "")) {
 	$ini = $_GET["inicio"];
 	$raz = $_GET["razao"];
 	$qtd = $_GET["quantidade"];
@@ -19,14 +21,14 @@ if (isset($_GET["inicio"]) && isset($_GET["razao"]) && isset($_GET["quantidade"]
 	}
 }
 
-	if (!isset ($_GET["inicio"])) { //se for diferente do que o isset quer (receber algum valor), passara a mensagem mto top
+	if (!isset ($_GET["inicio"])|| $_GET["inicio"] == "") { //se for diferente do que o isset quer (receber algum valor), passara a mensagem mto top
 	echo "Você esqueceu de passar o inicio" . "<br>";
 }
 
-	if (!isset ($_GET["razao"])) {
+	if (!isset ($_GET["razao"])|| $_GET["razao"] == "") {
 	echo "Você esqueceu de passar a razão" . "<br>";
 }
 
-	if (!isset ($_GET["quantidade"])) {
+	if (!isset ($_GET["quantidade"]) || $_GET["quantidade"] == "") {
 	echo "Você esqueceu de passar a quantidade" . "<br>";
 }
