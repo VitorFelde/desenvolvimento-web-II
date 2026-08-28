b5961829b5a63c8714ce6f71fbd22d8a311c5c18
Vitor Felde <feldevitor@gmail.com>
2026-08-27 22:06:17 -0300

    Add files via upload

---
+++ atividade 06 - cadastro com persistencia de dados/livro_excluir.php
@@ -0,0 +1,15 @@
+<?php
+
+//1 - receber o id do livro
+
+//2 - buscar os livros existentes no arquivo JSON
+
+//3 - encontrar o indice do livro no array 
+
+//4 - executar a funcao excluir
+
+//array_splice ( , )
+
+//5 - salvar os dados no arquico JSON
+
+//6 - redirecionar para os livros.php
---
+++ atividade 06 - cadastro com persistencia de dados/livros.php
@@ -0,0 +1,120 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once("persistencia.php");
+
+//buscar os livros ja cadastrados do arquivo JSON
+$livros = buscarDados("livros.json");
+
+if (isset($_POST["titulo"])) {
+
+$titulo = $_POST["titulo"];
+$genero = $_POST["genero"];
+$paginas = $_POST["qtd_paginas"];
+
+
+$livro = array("id" => uniqid(), "titulo" => $titulo, "genero" => $genero, "paginas" => $paginas);
+
+array_push($livros, $livro);
+
+
+salvarDados ($livros, "livros.json");
+
+header("location: livros.php");      
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de livros</title>
+</head>
+<body>
+
+<h1>Cadastro de livros</h1>
+
+<h3>Cadastre seu livro aqui</h3>
+<form method="POST" action="" >
+    <input type="text" name="titulo" id="titulo" 
+        placeholder="Informe o título"  />
+    
+    <br><br> 
+
+    <select name="genero" id="genero">
+        <option value="">--Selecione o gênero--</option>
+        <option value="D">Drama</option>
+        <option value="F">Ficção</option>
+        <option value="R">Romance</option>
+        <option value="O">Outro</option>
+    </select>
+    <br><br>
+
+    <input type="number" name="qtd_paginas" id="qtd_paginas" 
+        placeholder="Informe o número de páginas">
+    <br><br>
+
+    <input type="submit" value="Enviar" />
+</form>
+
+<h3>Livros cadastrados</h3>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Título</th>
+        <th>Gênero</th>
+        <th>Quant. Páginas</th>
+        <th>Excluir</th>
+    </tr>
+
+    <?php 
+    
+    foreach ($livros as $l): ?>
+
+    <tr>
+        <td><?php echo $l["id"]; ?></td>
+        <td><?php echo $l["titulo"]; ?></td>
+        <td><?php 
+        
+        if ($l["genero"] == 'D') {
+            echo "Drama";
+        }
+        
+        else if ($l["genero"] == 'F') {
+            echo "Ficção";
+        } 
+        
+        else if ($l["genero"] == 'R') {
+            echo "Romance";
+        }
+        else if ($l["genero"] == 'O') {
+            echo "Outro";
+        }
+
+        ?></td>
+        <td><?php echo $l["paginas"]; ?></td>
+        <td><a href="livro_excluir.php?id=<?php echo $l["id"];?>">Excluir</a></td>
+    
+    </tr>
+
+    <?php endforeach; ?>
+
+
+    
+    ?>
+
+
+
+
+</table>
+
+</body>
+</html>
