4b47d0579ee6fdbd486594be81ef3b5b684577a6
Vitor Felde <feldevitor@gmail.com>
2026-09-03 22:09:12 -0300

    Update livro_excluir.php

--- atividade 06 - cadastro com persistencia de dados/livro_excluir.php
+++ atividade 06 - cadastro com persistencia de dados/livro_excluir.php
@@ -1,4 +1,45 @@
 <?php
+include_once("persistencia.php");
+
+//1 - receber o id do livro
+
+if (!isset($_GET["id"])){
+    echo "Parametro ID nao informado";
+    exit; //serve para fechar a paradinha
+}
+
+$id = $_GET["id"];
+
+//2 - buscar os livros existentes no arquivo JSON
+
+$livros = buscarDados("livros.json");
+
+
+//3 - encontrar o indice do livro no array 
+$i = 0;
+foreach ($livros as $s) {
+    if ($s["id"] == $id) {
+        break;
+    }
+
+    $i++;
+
+}
+
+//4 - executar a funcao excluir
+//array_splice ( , )
+
+array_splice ($livros, $i, 1);
+
+
+//5 - salvar os dados no arquico JSON
+
+salvarDados($livros, "livros.json");
+
+//6 - redirecionar para os livros.php
+
+header("location: livros.php");      
+<?php
 
 //1 - receber o id do livro
 
